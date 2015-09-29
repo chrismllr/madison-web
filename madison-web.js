de1c3fc2f89d35a37cbde6401da4cd58cd46c84f
Chris Miller <chris@dialexa.com>
2015-09-29 12:56:41 -0500

    Beginnings of header/intro scroll binding

--- madison-web.js
+++ madison-web.js
@@ -4,8 +4,43 @@ if (Meteor.isClient) {
   Template.Projects.helpers({
     projects: [
       {id: 1, title: 'Project One', colors: {primary: '#A7D7B0', secondary: '#5BA36A'}},
-      {id: 2, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}}
+      {id: 2, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}},
+      {id: 3, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}},
+      {id: 4, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}},
+      {id: 5, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}},
+      {id: 6, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}},
+      {id: 7, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}},
+      {id: 8, title: 'Project Two', colors: {primary: '#CBE7F5', secondary: '#07B5E5'}}
     ]
+  });
+
+  Template.Header.onRendered(function() {
+    var application = {
+      init: function() {
+        this.cacheDom();
+        this.scrollEvents();
+      },
+
+      cacheDom: function() {
+        this.$window = $(window);
+        this.$siteHeader = $('#site-header');
+      },
+
+      scrollEvents: function() {
+        var _this = this;
+        var height = this.$siteHeader.innerHeight();
+
+        this.$window.on('scroll', function() {
+          if (_this.$window.scrollTop() > height) {
+            _this.$siteHeader.addClass('scrolled-past');
+          } else {
+            _this.$siteHeader.removeClass('scrolled-past');
+          }
+        });
+      }
+    };
+
+    application.init();
   });
 }
 
@@ -20,7 +55,4 @@ Router.configure({
 
 Router.route('/', function () {
   this.layout('Application');
-  this.render('Projects');
-  this.render('HeaderTemp', {to: 'header'});
-  this.render('Footer', {to: 'footer'});
 });
